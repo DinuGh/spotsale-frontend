f393ceef212cce9ecc5c3098cc5d434f8e144edc
Ali-Arani <94089011+Ali-Arani@users.noreply.github.com>
2022-11-27 11:39:31 -0500

    pre deploy

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+// import { Component } from "@angular/core";
+// @Component({
+//   selector: "app",
+//   template: "<router-outlet></router-outlet>"
+// })
+// export class AppComponent { }
+
+//=== Original ===//
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ src/app/model/product.model.ts
@@ -0,0 +1,32 @@
+export class Product {
+    //Constructor
+    constructor(
+        public _id?: string,
+        public category?: string,
+        public title?: string,
+        public description?: string,
+        public condition?: string,
+        public imageURL?: [string],
+        public price?: number,
+        public sold?: boolean,
+        public enable?: boolean,
+        public deliveryMethod?: string,
+        public creationDate?: string,
+        public publishedDate?: string,
+        public expiryDate?: string,
+        public userName?: string,
+        public questionAnswer?: [ {question: string, answer: string} ],
+        // Adds relationship with User
+        public owner?: string
+    ) { }
+
+    //Methods
+    clear() {
+        this._id = null;
+        this.category = this.title = this.description = null;
+        this.condition = this.imageURL = this.price = null;
+        this.sold = this.enable = false;
+        this.deliveryMethod = this.creationDate = this.publishedDate = null;
+        this.expiryDate = this.userName = this.questionAnswer = null;
+    }
+}
